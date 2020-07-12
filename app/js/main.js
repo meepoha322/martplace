820c997d7a6b0fc97d6ab71ba6c5bcfaa759c64c
baydack <vasyabaydack0@gmail.com>
2020-07-12 20:43:47 +0300

    section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,19 @@
 $(function(){
 	
+	$(".rate-star").rateYo({
+		rating: 4.5,
+		starWidth: "15px",
+		ratedFill: "#ffc000",
+		readOnly: true
+	});
+	
+	$('.products-slider__box').slick({
+
+		
+  });
+
+	var mixer = mixitup('.products-new__item');
+
 
 	
 });
